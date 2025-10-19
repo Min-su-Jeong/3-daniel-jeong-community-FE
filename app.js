b9ece392ee2350c9734ea07dd94a4bea84249a61
Min-su-Jeong <minsu1255@naver.com>
2025-10-19 16:40:15 +0900

    feat: dotenv 추가

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
