88cb07f5aacd6d2a1e7c9abd203a5067cb20404a
Min-su-Jeong <minsu1255@naver.com>
2025-10-19 15:42:06 +0900

    chore: ES6 모듈 설정

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
