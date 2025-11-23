3524dd5cceaeaa661d83004ee9f0bb8749aeadea
Min-su-Jeong <minsu1255@naver.com>
2025-11-23 16:42:05 +0900

    chore: 이미지 최대 용량 수정
    - 1개의 이미지당 5MB로 제한

--- public/utils/constants/image.js
+++ public/utils/constants/image.js
@@ -1,7 +1,7 @@
 /* 이미지 관련 상수 */
 export const IMAGE_CONSTANTS = Object.freeze({
     MAX_IMAGES: 10,
-    MAX_IMAGE_SIZE: 10 * 1024 * 1024, // 10MB
+    MAX_IMAGE_SIZE: 5 * 1024 * 1024, // 5MB
     ACCEPT: 'image/jpeg,image/jpg,image/png,image/gif,image/webp'
 });
 
